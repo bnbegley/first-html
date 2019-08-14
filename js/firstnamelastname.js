79737c055e930e52beac7513145ffa81565a3980
brail33na11@gmail.com <brail33na11@gmail.com>
2019-08-14 16:24:03 -0400

    initial commit

---
+++ js/firstnamelastname.js
@@ -0,0 +1,9 @@
+// console.log("Connected to the JS file."); this is just to help show us if the JS and HTML are correctly connected
+
+
+
+function firstlastname(){
+var firstname = document.getElementById("firstname").value;
+var lastname = document.getElementById("lastname").value;
+console.log(firstname, lastname);
+}
